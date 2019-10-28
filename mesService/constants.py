7a852141c71e6911013448cedf56c45a19750e14
王江桥 <248788461@qq.com>
2019-10-28 20:58:10 +0800

    item接口更新

--- mesService/constants.py
+++ mesService/constants.py
@@ -5,3 +5,11 @@
 # @email: luguang.huang@mabotech.com
 
 URL_PREFIX = '/api/v1/'
+
+# 物料类型
+PRODUCTINVENTORYTYPE_ENUM = {
+    'BFCEC_半成品': 110,
+    'BFCEC_采购件': 120,
+    'BFCEC_虚拟件': 130,
+    'BFCEC_通用类型': 100
+}
