b33c395a7a4bd4be55996e5d4abb8e9ebdff0fcd
王江桥 <jiangqiao.wang@techmabo.com>
2019-12-30 08:58:38 +0800

    工单排序和欠产脚本

---
+++ mesService/modules/JobTask/job_3_wip_badcrop.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# @createTime    : 2019/12/27
+# @author  : 王江桥
+# @fileName: job_3_wip_badcrop.py
+# @email: jiangqiao.wang@mabotech.com
+
+import sys
+sys.path.append(r'C:\Users\mabot\Desktop\BFCEC\foton\mesService')
+
+from mesService import config_dict
+from mesService.lib.pgwrap.db import connection
+
+
+class WipBadcrop(object):
+
+    def __init__(self):
+        self.db = self.create_conn('development')
+
+    def execteDatabase(self):
+        """调用存储过程"""
+        sql = "select wip_badcrop();"
+        try:
+            ret = self.db.query(sql)
+            print(ret)
+        except Exception as e:
+            # current_app.logger.error(traceback.format_exc())
+            print(e)
+
+    def create_conn(self, config_name):
+        db_info = config_dict[config_name].DB_INFO
+        db = connection(db_info)
+        return db
+
+
+if __name__ == '__main__':
+    wd = WipBadcrop()
+    wd.execteDatabase()
---
+++ mesService/modules/JobTask/job_4_wip_sequence.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# @createTime    : 2019/12/27
+# @author  : 王江桥
+# @fileName: job_4_wip_sequence.py
+# @email: jiangqiao.wang@mabotech.com
+
+import sys
+sys.path.append(r'C:\Users\mabot\Desktop\BFCEC\foton\mesService')
+
+from mesService import config_dict
+from mesService.lib.pgwrap.db import connection
+
+
+class WipSequence(object):
+
+    def __init__(self):
+        self.db = self.create_conn('development')
+
+    def execteDatabase(self):
+        """调用存储过程"""
+        sql = "select wip_sequence();"
+        try:
+            ret = self.db.query(sql)
+            print(ret)
+        except Exception as e:
+            # current_app.logger.error(traceback.format_exc())
+            print(e)
+
+    def create_conn(self, config_name):
+        db_info = config_dict[config_name].DB_INFO
+        db = connection(db_info)
+        return db
+
+
+if __name__ == '__main__':
+    wd = WipSequence()
+    wd.execteDatabase()
