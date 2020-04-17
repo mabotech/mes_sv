68f71b3452bd469ecb930bba3f8e2658912283f7
HuangLuGuang <835923123@qq.com>
2020-04-17 14:15:01 +0800

    Add Dockerfile And SOA XML Format

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3.7.7-slim-buster
+
+WORKDIR /messervice
+COPY requestments.txt /messervice
+
+RUN apt-get update \
+    && apt install -y libpq-dev \
+    && apt install -y gcc \
+	&& pip install --no-cache-dir -r requestments.txt
+
+EXPOSE 5000
+#CMD ["uwsgi", "--ini", "uwsgi.ini"]
+CMD ["python", "app_main.py"]
+
+# docker run -d --name messervice -v /home/test01/messervice:/messervice -p 5000:5000  messervice
--- mesService/modules/ERPInterface/mes_to_erp/iac/send_iac.py
+++ mesService/modules/ERPInterface/mes_to_erp/iac/send_iac.py
@@ -10,22 +10,46 @@ from mesService import config_dict
 from mesService.lib.pgwrap.db import connection
 
 
-class IacToXml(object):
+class Iac(object):
     def __init__(self):
-        self.root = etree.Element('root')
         self.db = self.create_conn('development')
         self.url = ''
 
-    def dict_to_xml(self, dict_data):
+    def format_soa_xml(self, iac_xml):
+        soa_format_xml = """<sen:SendSupplierCurrentAccountServiceBal xmlns:sen="http://www.foton.com.cn/SendSupplierCurrentAccountBal">
+	<sen:DATA>
+		<![CDATA[
+			<DATA>
+				<HEAD>
+					<BIZTRANSACTIONID>SAP_SYC_904_2017061010031100</BIZTRANSACTIONID>
+					<COUNT>3</COUNT>
+					<CONSUMER>SAP</CONSUMER>
+					<SRVLEVEL>1</SRVLEVEL>
+					<ACCOUNT>SAP</ACCOUNT>
+					<PASSWORD>SAP1509030</PASSWORD>
+				</HEAD>
+				<LIST>
+					<ITEM>
+						{iac_xml}
+					</ITEM>
+				</LIST>
+			</DATA>
+		]]>
+	</sen:DATA>
+</sen:SendSupplierCurrentAccountServiceBal>"""
+        return soa_format_xml.format(iac_xml = iac_xml)
 
+    def dict_to_xml(self, dict_data):
+        root = etree.Element('root')
         for k, v in dict_data.items():
-            node = etree.SubElement(self.root, k.capitalize())
+            node = etree.SubElement(root, k.capitalize())
             if v:
                 node.text = str(v)
 
-        tree = etree.ElementTree(self.root)
+        tree = etree.ElementTree(root)
         # tree.write('text.xml', pretty_print=True, xml_declaration=True, encoding='utf-8')
-        xml_str = etree.tostring(tree, encoding='utf-8', pretty_print=True)
+        xml_str = etree.tostring(tree, encoding='utf-8', pretty_print=True).decode()
+        # xml_str = etree.tostring(tree, encoding='utf-8').decode()
         return xml_str
 
     def get_iac_data(self):
@@ -54,9 +78,11 @@ class IacToXml(object):
 
 if __name__ == '__main__':
 
-    obj = IacToXml()
-    dataset = obj.get_iac_data()
+    iac = Iac()
+    dataset = iac.get_iac_data()
     if dataset:
         for data in dataset:
-            xml = obj.dict_to_xml(data)
+            xml = iac.dict_to_xml(data)
+            soa_xml = iac.format_soa_xml(xml)
+            # TODO
             # obj.set_to_erp(xml)
